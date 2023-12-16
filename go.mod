3659abbe46daf7a4a91d13c1406b0e524d8be303
vamshi-97 <vamshikrishna0997@gmail.com>
2023-12-17 01:35:28 +0530

    added few changes

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/vamshi1997/go-web-app
 
 go 1.21.4
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github/joho/godotenv"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	buildAndRunCommnad := "go build && ./go-web-app"
+	fmt.Printf("welcome to the simple server..., use this command to run application %v \n", buildAndRunCommnad)
+
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("Port is not found in environment")
+	}
+	fmt.Printf("Server listening to Port: %v", portString)
 }
